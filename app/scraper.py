4ffce40e8955a3b22b5e3796c0397cea88805d64
Sandeep Parajuli <sandeepparajuli96@gmail.com>
2025-11-01 19:33:18 +0545

    html and cookies

--- app/scraper.py
+++ app/scraper.py
@@ -4,7 +4,7 @@ import random
 import urllib.parse
 from typing import List
 from camoufox.async_api import AsyncCamoufox
-
+from fastapi import Response
 from app.helpers import load_state, save_state
 
 
@@ -58,3 +58,36 @@ async def scrape_links(
 
     finally:
         await page.close()
+
+
+async def scrape_url(url: str, browser: AsyncCamoufox) -> Response:
+    """
+    Scrape a single page with JS rendered content.
+    Returns HTML with cookies in headers.
+    """
+    try:
+        page = await browser.new_page()
+
+        response = await page.goto(url, timeout=120000, wait_until="domcontentloaded")
+
+        if not response or response.status != 200:
+            status = response.status if response else "No Response"
+            raise Exception(f"Non-200 status: {status}")
+
+
+        await page.wait_for_timeout(2000)  # 2s for JS to finish
+
+        html = await page.content()
+        cookies = await page.context.cookies()
+        await page.close()
+
+        html_response = Response(content=html, media_type="text/html")
+        for cookie in cookies:
+            html_response.headers.append(
+                "Set-Cookie",
+                f"{cookie['name']}={cookie['value']};"
+            )
+        return html_response
+
+    except Exception as e:
+        raise Exception(f"Scraping failed: {e}")
